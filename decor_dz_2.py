c3459aef11ca73b911a11560f6ed15e04be7bbb0
Вячеслав Андрющенко <31081994@mail.ru>
2022-10-20 22:55:53 +0700

    fix dz_2

--- decor_dz_2.py
+++ decor_dz_2.py
@@ -1,22 +1,20 @@
 from functools import wraps
 import datetime
 
+def path(path_log):
+    def trace_decorator(some_function):  
+        @wraps(some_function)  # wraps подменяет метаданные функции
+        def new_function(*args, **kwargs):           
+            with open(f'{path_log}', 'a', encoding="UTF8") as f:
+                f.write(f'{datetime.datetime.now()}: Вызываем {some_function.__name__} c аргументами {args} и {kwargs}\n')
+                result = some_function(*args, **kwargs)
+                f.write(f'Вернули результат {result}\n\n\n')
+                return result
 
-def trace_decorator(some_function):  
-    @wraps(some_function)  # wraps подменяет метаданные функции
-    def new_function(*args, **kwargs, ):  
-        print('Введите путь до файла логов')  
-        path_log = input()    
-        with open(f'{path_log}', 'a', encoding="UTF8") as f:
-            f.write(f'{datetime.datetime.now()}: Вызываем {some_function.__name__} c аргументами {args} и {kwargs}\n')
-            result = some_function(*args, **kwargs)
-            f.write(f'Вернули результат {result}\n\n\n')
-            return result
+        return new_function
+    return trace_decorator
 
-    return new_function
-
-
-@trace_decorator
+@path(path_log='log.txt')
 def summator(a,b):
     return a + b
 
